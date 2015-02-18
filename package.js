580e664e1b0cdf1d867f515e68c9489536421729
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:26:38 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-table',
+  summary : 'Semantic UI - Table (official): Single component release of table',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Table.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
