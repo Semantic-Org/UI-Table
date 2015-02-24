da5f4a1e7e12ad967a01c9f41147756d068e0171
Pusher Robot <jack@semantic-ui.com>
2015-02-24 14:34:40 -0500

    Updated component to version 1.10.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-table',
   summary : 'Semantic UI - Table: Single component release',
-  version : '1.10.1',
+  version : '1.10.2',
   git     : 'git://github.com/Semantic-Org/UI-Table.git',
 });
 
