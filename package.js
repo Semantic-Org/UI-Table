b9a8a4a22d96779e5956f1796f476a5c2f4b0e6a
Jack Lukic <jack@semantic-ui.com>
2017-02-21 14:24:53 -0500

    Updated component to version 2.2.9

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-table',
   summary : 'Semantic UI - Table: Single component release',
-  version : '2.2.8',
+  version : '2.2.9',
   git     : 'git://github.com/Semantic-Org/UI-Table.git',
 });
 
